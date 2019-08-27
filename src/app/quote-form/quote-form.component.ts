993852369f50f7e8a83c9cc53a26cf8fbb90ba8f
GraceUmutesi <tesigrace98@gmail.com>
2019-08-27 07:09:55 +0200

    latest changes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirstQuote } from '../first-quote'
 
 @Component({
   selector: 'app-quote-form',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new FirstQuote(0,"","");//new Date() to be added
   constructor() { }
 
   ngOnInit() {
