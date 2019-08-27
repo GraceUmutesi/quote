b1b24672c91bbeeee8aad6c91827666bd627e84d
GraceUmutesi <tesigrace98@gmail.com>
2019-08-27 20:50:52 +0200

    votes

--- src/app/date-time.pipe.ts
+++ src/app/date-time.pipe.ts
@@ -7,8 +7,8 @@ export class DateTimePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
     let today:Date = new Date(); 
-       let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-        var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
+       let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+        var dateDifference = Math.abs(value - todayWithNoTime);
         const secondsInDay = 86400;
         var dateDifferenceSeconds = dateDifference*0.001; 
         var dateCounter = dateDifferenceSeconds/secondsInDay;
--- src/app/first-quote.ts
+++ src/app/first-quote.ts
@@ -1,4 +1,6 @@
 export class FirstQuote  {
+    public upVote:boolean;
+    public downVote:boolean;
     public showDetail: boolean;
     constructor(public id: number,public description: string,public detail: string,public completeDate: Date){
       this.showDetail=false;
